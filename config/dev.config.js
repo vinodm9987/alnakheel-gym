a23e8a5022f340a648473998fa9c6610fb5ed2a3
unknown <goutham015@gmail.com>
2021-01-15 11:40:15 +0530

    Added

--- config/dev.config.js
+++ config/dev.config.js
@@ -3,7 +3,7 @@ module.exports = {
   IP: "127.0.0.1:5000",
   DBNAME: "alnakheel-gym",
   DB: `mongodb://127.0.0.1:27017/alnakheel-gym`,
-  MODE: "DEV",
+  MODE: "PROD",
   BUILDPATH: "../build/index.html",
   DESIGNATION: ["Super Admin", "System Admin", "Member", "Employee", "Trainer"],
   SECRET: "PIXEl",
