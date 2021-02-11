6a323d632f5f7d866c77f015d66ad5b5ad964bf5
goutham001 <goutham015@gmail.com>
2021-02-11 15:30:01 +0530

    Added

--- server/cronjob/oneDayCronjob.js
+++ server/cronjob/oneDayCronjob.js
@@ -2,7 +2,7 @@ const cronjob = require('node-cron');
 
 const { logger: { logger } } = require('../../config');
 
-const { checkPackageExpiry, checkStockExpiry, upgradeMember,
+const { checkPackageExpiry, checkStockExpiry, upgradeMember, checkAssetsExpiry,
     checkContractExpiry, checkVisaExpiry, checkFreezeMember } = require('./jobs')
 
 //
@@ -14,6 +14,7 @@ exports.oneDayCronJob = () => {
             await checkContractExpiry();
             await checkVisaExpiry();
             await checkFreezeMember();
+            await checkAssetsExpiry();
             await upgradeMember();
         } catch (error) {
             logger.error(error);
