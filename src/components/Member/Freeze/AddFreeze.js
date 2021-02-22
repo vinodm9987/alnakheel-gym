1310311c758c9f511e0855e47309723af5a39391
ansar.a <ansar.a@pixelmindit.com>
2021-02-22 11:56:30 +0530

    changes

--- src/components/Member/Freeze/AddFreeze.js
+++ src/components/Member/Freeze/AddFreeze.js
@@ -978,87 +978,86 @@ class AddFreeze extends Component {
         {/* --------------Receipt Modal Ends-=--------------- */}
 
         {packageReceipt &&
-          <div className="PageBillWrapper d-none">
-            <div style={{ width: "450px", padding: "15px", margin: "auto" }} id="newPrint">
+          <div className="PageBillWrapper d-none" id="newPrint">
+            <div style={{ width: "80mm", padding: "4px", margin: "auto" }}>
               <div style={{ display: "flex", justifyContent: "center" }}>
                 <img src={packageReceipt.memberId.branch.avatar ? `${PRODIP}/${packageReceipt.memberId.branch.avatar.path}` : ''} width="200" style={{ width: "100px" }} alt="" />
               </div>
-              <h5 style={{ textAlign: "center", margin: "19px 0" }}>{t('Tax Invoice')}</h5>
-              <p style={{ textAlign: "center", margin: "0 0 10px 0" }}>
+              <h5 style={{ textAlign: "center", margin: "19px 0px 9px 0px", fontSize: "19px" }}>{t('Tax Invoice')}</h5>
+              <p style={{ textAlign: "center", margin: "0 0 10px 0", fontSize: "14px" }}>
                 <span>{packageReceipt.memberId.branch.memberId.branchName}</span><br />
                 <span>{packageReceipt.memberId.branch.address}</span><br />
-                {/* <span>Road/Street 50, Samaheej,</span><br /> */}
-                {/* <span>Block 236, Bahrain,</span><br /> */}
-                <span>{t('Tel')} : {packageReceipt.memberId.branch.telephone}</span><br />
+              </p>
+              <p style={{ textAlign: "center", margin: "0 0 10px 0", fontSize: "14px" }}>
+                <span>{t('Tel')} : {packageReceipt.memberId.branch.telephone}</span>
               </p>
               <p style={{ textAlign: "center", margin: "0 0 10px 0" }}>{t('VAT Reg No')} - {packageReceipt.memberId.branch.vatRegNo}</p>
               <p style={{ display: "flex", justifyContent: "space-between", margin: "0" }}>
-                <span style={{ padding: "2px", fontSize: "14px" }}>{dateToDDMMYYYY(new Date())} {dateToHHMM(new Date())}</span>
-                <span style={{ padding: "2px", fontSize: "14px" }}>{t('Bill No')}:{packageReceipt.orderNo}</span>
+                <span>{dateToDDMMYYYY(new Date())} {dateToHHMM(new Date())}</span>
+                <span style={{ width: "4px", height: "4px" }}></span>
+                <span>{t('Bill No')}:{packageReceipt.orderNo}</span>
               </p>
               <div>
-                <p style={{ display: "flex", textAlign: "center", justifyContent: "space-between" }}>
-                  <span>{t('ID:')} <span style={{ padding: "10px" }}>{packageReceipt.memberId.memberId}</span></span>
-                  <span>{t('Mob:')} <span style={{ padding: "10px" }}>{packageReceipt.memberId.mobileNo}</span></span>
+                <p style={{ display: "flex", textAlign: "center", justifyContent: "space-between", fontSize: "14px" }}>
+                  <span style={{ display: "flex" }}>
+                    <span>{t('ID')}</span><span style={{ padding: "0 4px" }}>:</span><span>{packageReceipt.memberId.memberId}</span>
+                  </span>
+                  <span style={{ display: "flex" }}>
+                    <span>{t('Mob')}</span><span style={{ padding: "0 4px" }}>:</span><span>{packageReceipt.memberId.mobileNo}</span>
+                  </span>
                 </p>
-                <p style={{ display: "flex", textAlign: "center", justifyContent: "center", marginTop: "0" }}>
+                <p style={{ display: "flex", textAlign: "center", justifyContent: "center", marginTop: "0", fontSize: "14px" }}>
                   <span>{packageReceipt.memberId.credentialId.userName}</span>
                 </p>
               </div>
               {/* <p style={{ textAlign: "right", margin: "0 0 10px 0" }}>66988964</p> */}
-              <table style={{ width: "100%" }}>
+              <table style={{ width: "100%", fontSize: "14px" }}>
                 <tbody>
                   <tr style={{ borderTop: "1px dashed #000" }}>
-                    <td>{t('Type')}</td>
-                    <td>{t('From Date')}</td>
-                    <td>{t('To Date')}</td>
+                    <td style={{ borderTop: "1px dashed #000", borderBottom: "1px dashed #000", width: "50px" }}>{t('Type')}</td>
+                    <td style={{ borderTop: "1px dashed #000", borderBottom: "1px dashed #000" }}>{t('From Date')}</td>
+                    <td style={{ borderTop: "1px dashed #000", borderBottom: "1px dashed #000", width: "50px", textAlign: "center" }}>{t('To Date')}</td>
                   </tr>
-                  {/* <tr style={{ borderTop: "1px dashed #000" }}>
-                  <td>1</td>
-                  <td>3 Month</td>
-                  <td>26-Dec-19</td>
-                  <td>13-Sep-20</td>
-                </tr> */}
                   <tr>
-                    <td>Freezed</td>
+                    <td>{t('Freezed')}</td>
                     <td>{dateToDDMMYYYY(fromDate)}</td>
                     <td>{dateToDDMMYYYY(toDate)}</td>
                   </tr>
                 </tbody>
               </table>
-              <table style={{ width: "100%", textAlign: "right", borderTop: "1px dashed #000", borderBottom: "1px dashed #000" }}>
+              <table style={{ width: "100%", textAlign: "right", borderTop: "1px dashed #000", borderBottom: "1px dashed #000", fontSize: "14px" }}>
                 <tbody>
                   <tr>
-                    <td style={{ textAlign: "right", padding: "4px 4px 0 4px", width: "100%" }}>{t('Amount Total')} {this.props.defaultCurrency}: </td>
+                    <td style={{ textAlign: "right", padding: "4px 4px 0 4px", width: "100%" }}>{t('Amount Total')} {this.props.defaultCurrency} : </td>
                     <td style={{ textAlign: "right", padding: "4px 0px 0 0px" }}>{parseFloat(subTotal).toFixed(3)}</td>
                   </tr>
                   {parseFloat(totalVat) ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('VAT')} {this.props.defaultCurrency}: </td>
+                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('VAT')} {this.props.defaultCurrency} : </td>
                       <td style={{ textAlign: "right", padding: "0" }}>{parseFloat(totalVat).toFixed(3)}</td>
                     </tr>
                     : <tr></tr>}
                   {parseFloat(digital) ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('Digital')} {this.props.defaultCurrency}: </td>
+                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('Digital')} {this.props.defaultCurrency} : </td>
                       <td style={{ textAlign: "right", padding: "0" }}>5{parseFloat(digital).toFixed(3)}</td>
                     </tr>
                     : <tr></tr>}
                   {parseFloat(cash) ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('Cash')} {this.props.defaultCurrency}: </td>
+                      <td style={{ textAlign: "right", padding: "0 4px", width: "100%" }}>{t('Cash')} {this.props.defaultCurrency} : </td>
                       <td style={{ textAlign: "right", padding: "0" }}>5{parseFloat(cash).toFixed(3)}</td>
                     </tr>
                     : <tr></tr>}
                   {parseFloat(card) ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Card')} {this.props.defaultCurrency}: </td>
+                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Card')} {this.props.defaultCurrency} : </td>
                       <td style={{ textAlign: "right", padding: "0px 0px 4px 0px" }}>{parseFloat(card).toFixed(3)}</td>
                     </tr>
                     : <tr></tr>}
                   {parseFloat(this.state.cheque) ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Cheque')} {this.props.defaultCurrency}: </td>
+                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Cheque')} {this.props.defaultCurrency} : </td>
                       <td style={{ textAlign: "right", padding: "0px 0px 4px 0px" }}>{parseFloat(this.state.cheque).toFixed(3)}</td>
                     </tr>
                     : <tr></tr>}
@@ -1081,22 +1080,22 @@ class AddFreeze extends Component {
                     </tr>
                     : <tr></tr>}
                   <tr>
-                    <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Grand Total')} {this.props.defaultCurrency}: </td>
+                    <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Grand Total')} {this.props.defaultCurrency} : </td>
                     <td style={{ textAlign: "right", padding: "0px 0px 4px 0px" }}>{parseFloat(totalAmount).toFixed(3)}</td>
                   </tr>
                   <tr>
-                    <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Paid Amount')} {this.props.defaultCurrency}: </td>
+                    <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Paid Amount')} {this.props.defaultCurrency} : </td>
                     <td style={{ textAlign: "right", padding: "0px 0px 4px 0px" }}>{parseFloat(totalAmount).toFixed(3)}</td>
                   </tr>
                   {this.state.cardNumber ?
                     <tr>
-                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Card last four digit')} :</td>
+                      <td style={{ textAlign: "right", padding: "0px 4px 4px 4px", width: "100%" }}>{t('Card last four digit')} : </td>
                       <td style={{ textAlign: "right", padding: "0px 0px 4px 0px" }}>{this.state.cardNumber}</td>
                     </tr>
                     : <tr></tr>}
                 </tbody>
               </table>
-              <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0" }}>
+              <div style={{ display: "flex", justifyContent: "space-between", margin: "10px 0", fontSize: "14px" }}>
                 <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
                   <div style={{ marginRight: "10px", justifyContent: "center" }}>
                     <img src={instaimg} alt="" style={{ width: "30px", height: "30px" }} />
@@ -1105,13 +1104,13 @@ class AddFreeze extends Component {
                   <QRCode value={`http://instagram.com/${packageReceipt.memberId.branch.instaId}/`} renderAs='svg' width="50" height="50" />
                 </div>
                 {packageReceipt.doneBy &&
-                  <span>{t('Served by')}: {packageReceipt.doneBy.userName}</span>}
+                  <span>{t('Served by')} : {packageReceipt.doneBy.userName}</span>}
               </div>
-              <p style={{ display: "flex", margin: "0 0 10px 0" }}>
+              <p style={{ display: "flex", margin: "0 0 10px 0", fontSize: "14px" }}>
                 <span>{t('NB')}:</span>
                 <span style={{ flexGrow: "1", textAlign: "center" }}>{t('Membership cannot be refunded or transferred to others.')}</span>
               </p>
-              <p style={{ textAlign: "center", margin: "0 0 10px 0" }}>{t('Thank You')}</p>
+              <p style={{ textAlign: "center", margin: "0 0 10px 0", fontSize: "14px" }}>{t('Thank You')}</p>
             </div>
           </div>
         }
