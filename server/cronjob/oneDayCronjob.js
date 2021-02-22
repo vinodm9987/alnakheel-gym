3f692582ad22224eb916a9fb97f9788cb86477bc
goutham001 <goutham015@gmail.com>
2021-02-22 14:06:01 +0530

    changes

--- server/cronjob/oneDayCronjob.js
+++ server/cronjob/oneDayCronjob.js
@@ -12,12 +12,54 @@ exports.oneDayCronJob = () => {
 
     cronjob.schedule("00 */22 * * *", async () => {
         try {
-            await checkPackageExpiry();
-            await checkStockExpiry();
-            await checkContractExpiry();
-            await checkVisaExpiry();
-            await checkAssetsExpiry();
             await upgradeMember();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
+            await checkStockExpiry();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
+            await checkContractExpiry();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
+            await checkVisaExpiry();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
+            await checkPackageExpiry();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
+            await checkAssetsExpiry();
+        } catch (error) {
+            logger.error(error);
+        }
+    });
+
+    cronjob.schedule("00 */22 * * *", async () => {
+        try {
             await checkInstallmentsPending();
         } catch (error) {
             logger.error(error);
